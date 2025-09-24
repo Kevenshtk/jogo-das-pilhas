6781b5ae2fce9ea84a3b55f21839d8cabc4e4d09
Keven <kevendicamargo@gmail.com>
2025-09-24 14:50:25 -0300

    separar código de interface com o de lógica

---
+++ public/js/controller/controller.js
@@ -0,0 +1,120 @@
+import {
+  regrasIgual,
+  regrasMaiorMenor,
+  regrasEigual,
+  regrasOuIgual,
+  regrasEouMaiorMenor,
+} from "../model/regras.js";
+
+let regraAtual = null;
+
+let nivelAnterior = 0;
+let nivelSelecionado;
+
+function getRegras(nivel) {
+  switch (nivel) {
+    case 0:
+      let regrasLivre = [
+        ...regrasIgual,
+        ...regrasMaiorMenor,
+        ...regrasEigual,
+        ...regrasOuIgual,
+        ...regrasEouMaiorMenor,
+      ];
+      return regrasLivre;
+      break;
+    case 1:
+      return regrasIgual;
+      break;
+    case 2:
+      return regrasMaiorMenor;
+      break;
+    case 3:
+      return regrasEigual;
+      break;
+    case 4:
+      return regrasOuIgual;
+      break;
+    case 5:
+      return regrasEouMaiorMenor;
+      break;
+  }
+}
+
+function getRegraAtual() {
+  return regraAtual;
+}
+
+function gerarAlternativas(regraCorreta) {
+  const regrasNivelSelecionado = getRegras(nivelSelecionado);
+
+  let alternativas = [regraCorreta.condicao];
+
+  while (alternativas.length < 4) {
+    let r =
+      regrasNivelSelecionado[
+        Math.floor(Math.random() * regrasNivelSelecionado.length)
+      ].condicao;
+    if (!alternativas.includes(r)) alternativas.push(r);
+  }
+
+  alternativas.sort(() => Math.random() - 0.5);
+
+  return alternativas;
+}
+
+function selecionarRegra() {
+  const regrasNivelSelecionado = getRegras(nivelSelecionado);
+
+  regraAtual =
+    regrasNivelSelecionado[
+      Math.floor(Math.random() * regrasNivelSelecionado.length)
+    ];
+
+  return regraAtual;
+}
+
+function getNivelDificuldadeAnterior() {
+  return nivelAnterior;
+}
+
+function setNivelDificuldadeAnterior(nivel) {
+  nivelAnterior = nivel;
+}
+
+function getNivelDificuldade() {
+  return nivelSelecionado;
+}
+
+function setNivelDificuldade(nivel) {
+  nivelSelecionado = nivel;
+}
+
+function checarSeparacao(resposta, imagensOriginais) {
+  const regraAtual = getRegraAtual();
+
+  const corretos = imagensOriginais.filter((d) =>
+    regraAtual.filtro({
+      id: d.id,
+      classList: { contains: (cls) => d.className.includes(cls) },
+    })
+  );
+  const idsCorretos = corretos.map((c) => c.id);
+
+  const acertou =
+    resposta.length === idsCorretos.length &&
+    resposta.every((id) => idsCorretos.includes(id));
+
+    return acertou;
+}
+
+export {
+  getNivelDificuldadeAnterior,
+  setNivelDificuldadeAnterior,
+  getNivelDificuldade,
+  setNivelDificuldade,
+  selecionarRegra,
+  gerarAlternativas,
+  getRegraAtual,
+  checarSeparacao,
+};
---
+++ public/js/view/view.js
@@ -0,0 +1,343 @@
+import {
+  getNivelDificuldadeAnterior,
+  setNivelDificuldadeAnterior,
+  getNivelDificuldade,
+  setNivelDificuldade,
+  selecionarRegra,
+  gerarAlternativas,
+  getRegraAtual,
+  checarSeparacao,
+} from "../controller/controller.js";
+
+let draggedItem = null;
+
+function inicializarDragAndDrop() {
+  const imgs = document.querySelectorAll("img");
+  imgs.forEach((img) => {
+    img.addEventListener("dragstart", dragStart);
+    img.addEventListener("dragend", dragEnd);
+  });
+
+  [pilhaPrincipal, pilhaSecundaria].forEach((pilha) => {
+    pilha.addEventListener("dragover", dragOver);
+    pilha.addEventListener("dragenter", dragEnter);
+    pilha.addEventListener("dragleave", dragLeave);
+    pilha.addEventListener("drop", drop);
+  });
+}
+
+function dragStart(e) {
+  draggedItem = e.target;
+  draggedItem.classList.add("dragging");
+}
+
+function dragEnd(e) {
+  draggedItem.classList.remove("dragging");
+  draggedItem = null;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  this.classList.remove("over");
+}
+
+function drop(e) {
+  this.classList.remove("over");
+  if (draggedItem) {
+    this.appendChild(draggedItem);
+  }
+}
+
+function esconderTela(tela) {
+  const telaSelecionada = document.getElementById(`${tela}`);
+  telaSelecionada.style.display = "none";
+}
+
+function voltarInicio() {
+  const telaInicio = document.getElementById("telaInicio");
+  const telaJogo = document.getElementById("telaJogo");
+  const telaManual = document.getElementById("telaManual");
+
+  telaInicio.style.display = "flex";
+  telaJogo.style.display = "none";
+  telaManual.style.display = "none";
+}
+
+function montarTelaManual() {
+  esconderTela("telaInicio");
+  const tela = document.getElementById("telaManual");
+  tela.style.display = "flex";
+
+  const btnVoltar = document.querySelector("#telaManual > .btn-voltar-inicio");
+  btnVoltar.onclick = voltarInicio;
+}
+
+function montarTelaDificuldade(modo) {
+  const tela = document.getElementById("telaDificuldade");
+  tela.style.display = "flex";
+
+  const titulo = document.querySelector("#telaDificuldade > .titulo");
+  titulo.textContent = modo === 1 ? "Descubra a Pilha" : "Monte a Pilha";
+}
+
+function alterarLegendaDificuldade(nivel) {
+  const info = [
+    { titulo: "Qualquer possibilidade de lógica.", desc: "" },
+    {
+      titulo: "Lógica com apenas uma condição:",
+      desc: "Uma das características da figura deve corresponder à característica sorteada.",
+    },
+    {
+      titulo: "Lógica com apenas uma condição:",
+      desc: "Uma das características da figura deve ser maior ou menor que a característica sorteada.",
+    },
+    {
+      titulo: "Lógica com duas condições:",
+      desc: "Para cada característica sorteada, ambas devem corresponder às características da figura.",
+    },
+    {
+      titulo: "Lógica com duas condições:",
+      desc: "Para cada característica sorteada, pelo menos uma deve corresponder às características da figura.",
+    },
+    {
+      titulo: "Lógica com duas condições:",
+      desc: "Para cada característica sorteada, elas podem ser tanto ambas quanto uma apenas maior ou menor que a característica da figura.",
+    },
+  ];
+
+  const nivelAnterior = getNivelDificuldadeAnterior();
+
+  if (nivel !== nivelAnterior) {
+    const btnAnterior = document.getElementById(`nivel-${nivelAnterior}`);
+    btnAnterior.style.color = "#ffff";
+    setNivelDificuldadeAnterior(nivel);
+  }
+
+  const titulo = document.querySelector(".info > .info-titulo");
+  const desc = document.querySelector(".info > .info-desc");
+  const btn = document.getElementById(`nivel-${nivel}`);
+  btn.style.color = "#01883eff";
+
+  titulo.textContent = info[nivel].titulo;
+  desc.textContent = info[nivel].desc;
+}
+
+function salvarDificuldade(nivel) {
+  setNivelDificuldade(nivel);
+  alterarLegendaDificuldade(nivel);
+}
+
+const modal = document.getElementById("modal");
+const divManual = document.querySelector(".modal-manual");
+const divConcicao = document.querySelector(".modal-condicao");
+const divAlternativas = document.querySelector(".alternativas");
+const btnAbrirModal = document.querySelector(".btn-abrir-modal");
+const btnFecharModal = document.querySelector(".btn-fechar-modal");
+
+function abrirModal(opt) {
+  modal.style.display = "flex";
+  btnAbrirModal.style.display = "none";
+
+  if (opt === "info") {
+    divManual.style.display = "block";
+    divConcicao.style.display = "none";
+  } else {
+    divConcicao.style.display = "block";
+    divManual.style.display = "none";
+  }
+}
+
+function fecharModal() {
+  modal.style.display = "none";
+  btnAbrirModal.style.display = "block";
+}
+
+function montarModalInfo(modo) {
+  const titulo = document.querySelector(".modal-manual > .modal-titulo");
+
+  const ulModal = document.querySelector(".modal-manual > ul");
+
+  if (modo === 1) {
+    titulo.textContent = "DESCUBRA A PILHA";
+    const liManual = document.querySelectorAll("#descubra ul li");
+    ulModal.innerHTML = "";
+
+    liManual.forEach((li) => {
+      const liModal = document.createElement("li");
+      if (
+        li.textContent.includes("Pilha da esquerda") ||
+        li.textContent.includes("Pilha da direita")
+      ) {
+        liModal.classList.add("decoracao");
+      }
+
+      liModal.textContent = li.textContent;
+      ulModal.appendChild(liModal);
+    });
+  } else if (modo === 2) {
+    titulo.textContent = "MONTE A PILHA";
+    const liManual = document.querySelectorAll("#monte ul li");
+    ulModal.innerHTML = "";
+
+    liManual.forEach((li) => {
+      const liModal = document.createElement("li");
+      liModal.textContent = li.textContent;
+      ulModal.appendChild(liModal);
+    });
+  }
+}
+
+function montarAlternativas(regraCorreta) {
+  divAlternativas.innerHTML = "";
+
+  const alternativas = gerarAlternativas(regraCorreta);
+
+  alternativas.forEach((opcao) => {
+    const btn = document.createElement("button");
+    btn.textContent = opcao;
+
+    btn.onclick = () => {
+      if (opcao === regraCorreta.condicao) {
+        alert("✅ Resposta correta!");
+        iniciarRodada(1);
+      } else {
+        alert("❌ Tente novamente!");
+      }
+
+      fecharModal();
+    };
+
+    divAlternativas.appendChild(btn);
+  });
+}
+
+function montarModalCondicao(modo, regra) {
+  const titulo = document.querySelector(".modal-condicao > .modal-titulo");
+
+  if (modo === 1) {
+    titulo.textContent = "Qual foi a lógica usada?";
+    montarAlternativas(regra);
+  } else if (modo === 2) {
+    divAlternativas.innerHTML = "";
+    titulo.textContent = `👉 Sua lógica é: ${regra.condicao}`;
+  }
+}
+
+function validarSeparacao() {
+  const imgsPrincipal = Array.from(pilhaPrincipal.querySelectorAll("img"));
+  const idsPrincipal = imgsPrincipal.map((i) => i.id);
+
+  const acertou = checarSeparacao(idsPrincipal, imagensOriginais);
+
+  if (acertou) {
+    alert("🎉 Você acertou a separação!");
+    iniciarRodada(2);
+    abrirModal();
+  } else {
+    alert("❌ Separação incorreta, tente de novo.");
+  }
+}
+
+function montarTelaJogo(modo) {
+  const tela = document.getElementById("telaJogo");
+  tela.style.display = "flex";
+
+  const titulo = document.querySelector("#telaJogo > .titulo");
+  const instrucao = document.querySelector("#telaJogo > .instrucao");
+  const btnVoltar = document.querySelector("#telaJogo > .btn-voltar-inicio");
+  const btnInfo = document.querySelector("#telaJogo > .btn-info");
+  const btn = document.querySelector("#telaJogo > .btn-responder");
+
+  btnVoltar.onclick = voltarInicio;
+  btnInfo.onclick = () => abrirModal("info");
+
+  if (modo == 1) {
+    titulo.textContent = "Descubra a Pilha";
+    instrucao.textContent =
+      "Observe como as figuras foram separadas e tente adivinhar a lógica usada na pilha esquerda!";
+    btn.textContent = "Responder";
+    btn.onclick = () => abrirModal("condicao");
+  } else {
+    titulo.textContent = "Monte a Pilha";
+    instrucao.textContent =
+      "Mova as figuras para a pilha da esquerda conforme a lógica!";
+    btn.textContent = "Verificar";
+    btn.onclick = validarSeparacao;
+
+    abrirModal("condicao");
+  }
+
+  montarModalInfo(modo);
+}
+
+btnFecharModal.onclick = fecharModal;
+
+btnAbrirModal.onclick = () => abrirModal();
+
+window.onclick = (e) => {
+  if (e.target === modal) fecharModal();
+};
+
+const pilhaPrincipal = document.getElementById("principal");
+const pilhaSecundaria = document.getElementById("secundaria");
+const imagensOriginais = [];
+
+document.querySelectorAll("#secundaria img").forEach((img) => {
+  imagensOriginais.push({ id: img.id, src: img.src, className: img.className });
+});
+
+function limparPilhas() {
+  pilhaPrincipal.innerHTML = "";
+  pilhaSecundaria.innerHTML = "";
+
+  imagensOriginais.forEach((dados) => {
+    let img = document.createElement("img");
+    img.src = dados.src;
+    img.id = dados.id;
+    img.className = dados.className;
+    img.draggable = true;
+
+    pilhaSecundaria.appendChild(img);
+  });
+}
+
+function iniciarRodada(modo) {
+  limparPilhas();
+
+  const regraAtual = selecionarRegra();
+
+  if (modo === 1) {
+    const imgs = Array.from(pilhaSecundaria.querySelectorAll("img"));
+
+    imgs.forEach((img) => {
+      if (regraAtual.filtro(img)) {
+        pilhaPrincipal.appendChild(img);
+      }
+    });
+
+    btnAbrirModal.style.display = "block";
+  } else {
+    btnAbrirModal.style.display = "block";
+
+    inicializarDragAndDrop();
+  }
+
+  montarModalCondicao(modo, regraAtual);
+}
+
+export {
+  esconderTela,
+  montarTelaManual,
+  montarTelaDificuldade,
+  salvarDificuldade,
+  montarTelaJogo,
+  iniciarRodada,
+};
