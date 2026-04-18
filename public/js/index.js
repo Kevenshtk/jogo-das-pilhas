3f442478396d14c922e3ce8a0c99eb23be2222a1
Keven <kevendicamargo@gmail.com>
2026-04-18 15:56:19 -0300

    adicionar vídeos tutorial

--- public/js/index.js
+++ public/js/index.js
@@ -29,3 +29,26 @@ function iniciarJogo(modo) {
 window.iniciarJogo = iniciarJogo;
 window.salvarDificuldade = salvarDificuldade;
 window.montarTelaManual = montarTelaManual;
+
+function abrirVideo(src) {
+  const modal = document.getElementById("modalVideo");
+  const videoElement = document.getElementById("playerVideo");
+  const sourceElement = document.getElementById("videoSource");
+  
+  sourceElement.src = src;
+  videoElement.load();
+  modal.style.display = "flex";
+  videoElement.play();
+}
+
+function fecharVideo() {
+  const modal = document.getElementById("modalVideo");
+  const videoElement = document.getElementById("playerVideo");
+  
+  modal.style.display = "none";
+  videoElement.pause();
+  videoElement.currentTime = 0;
+}
+
+window.abrirVideo = abrirVideo;
+window.fecharVideo = fecharVideo;
